03ca23b43df6f00f6f4fd81e5ee771c850e27486
Giri Putra Adhittana <girput01@icloud.com>
2022-03-19 14:56:53 +0700

    Hello

--- test/server_test.js
+++ test/server_test.js
@@ -16,7 +16,6 @@ describe('Test GET posts', () => {
                 .end((err, response) => {
                     response.should.have.status(200);
                     response.body.should.be.a('object');
-                    response.body.sh
                     done();
                 });
         });
